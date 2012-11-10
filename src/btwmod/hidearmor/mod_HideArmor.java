af2a8200e20d5d488f9b08f19b4c3aba6fc31847
Andre Mekkawi <github@andremekkawi.com>
2012-11-10 03:39:51 -0500

    Added mod for hiding worn armor.

---
+++ src/btwmod/hidearmor/mod_HideArmor.java
@@ -0,0 +1,213 @@
+package btwmod.hidearmor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.src.CommandBase;
+import net.minecraft.src.Entity;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.EntityPlayerMP;
+import net.minecraft.src.FCItemRefinedArmor;
+import net.minecraft.src.ICommandSender;
+import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.Packet5PlayerInventory;
+import net.minecraft.src.WorldServer;
+import net.minecraft.src.WrongUsageException;
+import btwmods.CommandsAPI;
+import btwmods.IMod;
+import btwmods.NetworkAPI;
+import btwmods.PlayerAPI;
+import btwmods.WorldAPI;
+import btwmods.io.Settings;
+import btwmods.network.IPacketHandlerListener;
+import btwmods.network.PacketHandlerEvent;
+import btwmods.player.IInstanceListener;
+import btwmods.player.InstanceEvent;
+import btwmods.player.InstanceEvent.METADATA;
+import btwmods.player.InstanceEvent.TYPE;
+
+public class mod_HideArmor extends CommandBase implements IMod, IPacketHandlerListener, IInstanceListener {
+	
+	private boolean plateAlwaysVisible = true;
+	
+	private Map<String, HideSettings> playerSettings = new HashMap<String, HideSettings>();
+	
+	private MinecraftServer server;
+	
+	@Override
+	public String getName() throws Exception {
+		return "Hide Armor";
+	}
+
+	@Override
+	public void init(Settings settings) throws Exception {
+		if (settings.isBoolean("platealwaysvisible")) {
+			plateAlwaysVisible = settings.getBoolean("platealwaysvisible");
+		}
+		
+		CommandsAPI.registerCommand(this, this);
+		NetworkAPI.addListener(this);
+		PlayerAPI.addListener(this);
+		server = MinecraftServer.getServer();
+	}
+
+	@Override
+	public void unload() throws Exception {
+		CommandsAPI.unregisterCommand(this);
+		NetworkAPI.removeListener(this);
+		PlayerAPI.removeListener(this);
+	}
+
+	@Override
+	public IMod getMod() {
+		return this;
+	}
+
+	@Override
+	public void onHandlePacket(PacketHandlerEvent event) {
+		if (event.getPacket() instanceof Packet5PlayerInventory) {
+			Packet5PlayerInventory packet = (Packet5PlayerInventory)event.getPacket();
+			
+			// Get the entity using the ID.
+			WorldServer world = server.worldServerForDimension(event.getPlayer().dimension);
+			Entity entity = world.getEntityByID(packet.entityID);
+			
+			// Only handle if the player referenced in the packet is different than the player receiving it.
+			if (!event.getPlayer().equals(entity) && entity instanceof EntityPlayerMP) {
+				EntityPlayerMP referencedPlayer = (EntityPlayerMP)entity;
+				
+				// Armor is always visible in PvP
+				if (!PlayerAPI.isPvPEnabled(referencedPlayer)) {
+					
+					// Get the referenced player's hide settings.
+					HideSettings settings = playerSettings.get(referencedPlayer.username);
+					System.err.println(playerSettings.size() + " " + referencedPlayer.username + " " + settings + " " + playerSettings.hashCode());
+					
+					// Only handle armor slots.
+					if (settings != null && packet.slot > 0 && settings.isHiddenForSlot(packet.slot)) {
+						ItemStack stack = referencedPlayer.getEquipmentInSlot(packet.slot);
+						if (stack != null && (!plateAlwaysVisible || !(Item.itemsList[stack.itemID] instanceof FCItemRefinedArmor))) {
+							event.replaceWithPacket(new Packet5PlayerInventory(packet.entityID, packet.slot, null));
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void instanceAction(InstanceEvent event) {
+		if (event.getType() == TYPE.METADATA_CHANGED && event.getMetadata() == METADATA.IS_PVP) {
+			WorldAPI.sendEntityEquipmentUpdate(event.getPlayerInstance());
+		}
+		else if (event.getType() == TYPE.READ_NBT) {
+			HideSettings settings = new HideSettings();
+			settings.helm = event.getNBTTagCompound().getBoolean("helm");
+			settings.chest = event.getNBTTagCompound().getBoolean("chest");
+			settings.legs = event.getNBTTagCompound().getBoolean("legs");
+			settings.boots = event.getNBTTagCompound().getBoolean("boots");
+			playerSettings.put(event.getPlayerInstance().username, settings);
+			System.err.println("Loading settings for " + event.getPlayerInstance().username + " into hash " + playerSettings.hashCode() + " (" + playerSettings.size() + " total)");
+		}
+		else if (event.getType() == TYPE.WRITE_NBT) {
+			HideSettings settings = playerSettings.get(event.getPlayerInstance().username);
+			if (settings != null) {
+				event.getNBTTagCompound().setBoolean("helm", settings.helm);
+				event.getNBTTagCompound().setBoolean("chest", settings.chest);
+				event.getNBTTagCompound().setBoolean("legs", settings.legs);
+				event.getNBTTagCompound().setBoolean("boots", settings.boots);
+			}
+		}
+	}
+
+	@Override
+	public String getCommandName() {
+		return "armor";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		return "/" + getCommandName() + " <hide|show> [<helm|chest|legs|boots> ...]";
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender sender) {
+		return sender instanceof EntityPlayer;
+	}
+
+	@Override
+	public List addTabCompletionOptions(ICommandSender sender, String[] args) {
+		if (sender instanceof EntityPlayer) {
+			if (args.length == 1) {
+				return getListOfStringsMatchingLastWord(args, new String[] { "hide", "show" });
+			}
+			else {
+				return getListOfStringsMatchingLastWord(args, new String[] { "helm", "chest", "legs", "boots" });
+			}
+		}
+		
+		return super.addTabCompletionOptions(sender, args);
+	}
+
+	@Override
+	public void processCommand(ICommandSender sender, String[] args) {
+		if (sender instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer)sender;
+			if (args.length >= 1 && args[0].matches("^(hide|show)$")) {
+				
+				// Get the settings, creating them if necessary.
+				HideSettings settings = playerSettings.get(player.username);
+				if (settings == null) {
+					playerSettings.put(player.username, settings = new HideSettings());
+				}
+				
+				boolean state = args[0].equalsIgnoreCase("hide");
+				
+				if (args.length == 1) {
+					settings.setAll(state);
+				}
+				else {
+					for (int i = 1; i < args.length; i++) {
+						if (args[i].equalsIgnoreCase("helm"))
+							settings.helm = state;
+						
+						else if (args[i].equalsIgnoreCase("chest"))
+							settings.chest = state;
+						
+						else if (args[i].equalsIgnoreCase("legs"))
+							settings.legs = state;
+						
+						else if (args[i].equalsIgnoreCase("boots"))
+							settings.boots = state;
+						
+						else
+							throw new WrongUsageException(getCommandUsage(sender), new Object[0]);
+					}
+				}	
+			}
+			else {
+				throw new WrongUsageException(getCommandUsage(sender), new Object[0]);
+			}
+		}
+	}
+	
+	private class HideSettings {
+		public boolean helm = false;
+		public boolean chest = false;
+		public boolean legs = false;
+		public boolean boots = false;
+		
+		public boolean isHiddenForSlot(int slot) {
+			System.err.println("isHiddenForSlot " + slot);
+			return true;
+		}
+		
+		public void setAll(boolean value) {
+			helm = chest = legs = boots = value;
+		}
+	}
+
+}
