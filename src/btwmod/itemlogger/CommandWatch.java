f319e23262698339fa05973788567bbee06fc2cc
Andre Mekkawi <github@andremekkawi.com>
2013-01-03 11:24:51 -0500

    Added proper 'wrong usage' messages to Item Logger's /watch command.

--- src/btwmod/itemlogger/CommandWatch.java
+++ src/btwmod/itemlogger/CommandWatch.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import btwmods.Util;
+import btwmods.commands.CommandBaseExtended;
 
-import net.minecraft.src.CommandBase;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.ICommandSender;
 import net.minecraft.src.Packet3Chat;
+import net.minecraft.src.WrongUsageException;
 
-public class CommandWatch extends CommandBase {
+public class CommandWatch extends CommandBaseExtended {
 
 	private final mod_ItemLogger mod;
 
@@ -24,7 +26,7 @@ public class CommandWatch extends CommandBase {
 
 	@Override
 	public void processCommand(ICommandSender sender, String[] args) {
-		if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
+		if (isStringMatch(args, 0, "list")) {
 			List<String> names = Arrays.asList(mod.getWatchedPlayers());
 			if (names.size() == 0) {
 				sender.sendChatToPlayer(mod.getName() + " is not watching any players.");
@@ -36,7 +38,10 @@ public class CommandWatch extends CommandBase {
 				}
 			}
 		}
-		else if (args.length == 2 && args[0].equalsIgnoreCase("add")) {
+		else if (isStringMatch(args, 0, "add")) {
+			if (args.length != 2)
+				throw new WrongUsageException("/" + getCommandName() + " add <username>", new Object[0]);
+				
 			if (mod.addWatchedPlayer(args[1].toLowerCase().trim())) {
 				sender.sendChatToPlayer(mod.getName() + " is now watching player '" + args[1].toLowerCase().trim() + "'.");
 			}
@@ -44,7 +49,10 @@ public class CommandWatch extends CommandBase {
 				sender.sendChatToPlayer(mod.getName() + " is already watching player '" + args[1].toLowerCase().trim() + "'.");
 			}
 		}
-		else if (args.length == 2 && args[0].equalsIgnoreCase("remove")) {
+		else if (isStringMatch(args, 0, "remove")) {
+			if (args.length != 2)
+				throw new WrongUsageException("/" + getCommandName() + " remove <username>", new Object[0]);
+			
 			if (mod.removeWatchedPlayer(args[1].toLowerCase().trim())) {
 				sender.sendChatToPlayer(mod.getName() + " is no longer watching player '" + args[1].toLowerCase().trim() + "'.");
 			}
@@ -52,17 +60,26 @@ public class CommandWatch extends CommandBase {
 				sender.sendChatToPlayer(mod.getName() + " is not watching player '" + args[1].toLowerCase().trim() + "'.");
 			}
 		}
+		else {
+			throw new WrongUsageException(getCommandUsage(sender), new Object[0]);
+		}
 	}
 
 	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		return "/" + getCommandName() + " <list|add|remove> [name]";
+		return "/" + getCommandName() + " (list | add | remove)";
 	}
 
 	@Override
 	public List addTabCompletionOptions(ICommandSender sender, String[] args) {
 		if (args.length == 1)
 			return getListOfStringsMatchingLastWord(args, new String[] { "list", "add", "remove" });
+		
+		else if (args.length == 2 && args[0].equalsIgnoreCase("add"))
+			return getListOfStringsMatchingLastWord(args, mod.getWatchedPlayers());
+		
+		else if (args.length == 2 && args[0].equalsIgnoreCase("remove"))
+			return getListOfStringsMatchingLastWord(args, MinecraftServer.getServer().getAllUsernames());
 			
 		return super.addTabCompletionOptions(sender, args);
 	}
