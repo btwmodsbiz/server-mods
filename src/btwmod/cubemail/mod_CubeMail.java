d47876c97441342e374e41883f86a1b6dd2e3861
Andre Mekkawi <github@andremekkawi.com>
2013-02-04 13:40:57 -0500

    More work done to Cube Mail.

--- src/btwmod/cubemail/mod_CubeMail.java
+++ src/btwmod/cubemail/mod_CubeMail.java
@@ -23,6 +23,8 @@ import btwmods.player.PlayerBlockEvent;
 public class mod_CubeMail implements IMod, IPlayerBlockListener {
 	
 	public static final Pattern toLine = Pattern.compile("^(?:To|Dear) ([0-9a-z_]{1,16}),", Pattern.CASE_INSENSITIVE);
+	
+	private Settings data = null;
 
 	@Override
 	public String getName() {
@@ -31,6 +33,7 @@ public class mod_CubeMail implements IMod, IPlayerBlockListener {
 
 	@Override
 	public void init(Settings settings, Settings data) throws Exception {
+		this.data = data;
 		PlayerAPI.addListener(this);
 	}
 
@@ -41,8 +44,7 @@ public class mod_CubeMail implements IMod, IPlayerBlockListener {
 
 	@Override
 	public IMod getMod() {
-		// TODO Auto-generated method stub
-		return null;
+		return this;
 	}
 
 	@Override
@@ -53,47 +55,13 @@ public class mod_CubeMail implements IMod, IPlayerBlockListener {
 			if (world != null && heldStack != null) {
 				Item held = heldStack.getItem();
 				if (held != null && held instanceof ItemFood && ((ItemFood)held).isWolfsFavoriteMeat()) {
-					int above = event.getWorld().getBlockId(event.getX(), event.getY() + 1, event.getZ());
-					if (above == Block.chest.blockID) {
-						TileEntityChest inventory = (TileEntityChest)world.getBlockTileEntity(event.getX(), event.getY() + 1, event.getZ());
-						if (inventory != null) {
-							boolean foundValidBook = true;
-							
-							for (int i = 0, count = inventory.getSizeInventory(); i < count; i++) {
-								ItemStack book = inventory.getStackInSlot(i);
-								if (book != null && book.getItem() == Item.writtenBook) {
-									NBTTagCompound tagCompound = book.getTagCompound();
-									if (ItemEditableBook.validBookTagContents(tagCompound)) {
-										NBTTagString title = (NBTTagString)tagCompound.getTag("title");
-										NBTTagString author = (NBTTagString)tagCompound.getTag("author");
-										NBTTagList pages = (NBTTagList)tagCompound.getTag("pages");
-										if (pages.tagCount() > 0) {
-											NBTTagString firstPage = (NBTTagString)pages.tagAt(0);
-											if (firstPage != null && firstPage.data != null) {
-												Matcher matcher = toLine.matcher(firstPage.data.trim());
-												if (matcher.matches()) {
-													String username = matcher.group(1);
-													if (username != null) {
-														foundValidBook = true;
-														inventory.setInventorySlotContents(i, null);
-														
-														// TODO: Send mail.
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							
-							if (foundValidBook) {
-								if (!((EntityPlayerMP)event.getPlayer()).theItemInWorldManager.isCreative()) {
-									heldStack.stackSize--;
-								}
-								
-								event.markHandled();
-							}
+					
+					if (processChest(event.getWorld(), event.getX(), event.getY() + 1, event.getZ())) {
+						if (!((EntityPlayerMP)event.getPlayer()).theItemInWorldManager.isCreative()) {
+							heldStack.stackSize--;
 						}
+						
+						event.markHandled();
 					}
 				}
 			}
@@ -101,7 +69,7 @@ public class mod_CubeMail implements IMod, IPlayerBlockListener {
 	}
 	
 	private boolean processChest(World world, int x, int y, int z) {
-		if (world.getBlockId(x, y + 1, z) == Block.chest.blockID) {
+		if (world.getBlockId(x, y, z) == Block.chest.blockID) {
 			TileEntityChest inventory = (TileEntityChest)world.getBlockTileEntity(x, y, z);
 			if (inventory != null) {
 				return processInventory(inventory);
@@ -117,9 +85,11 @@ public class mod_CubeMail implements IMod, IPlayerBlockListener {
 		for (int i = 0, count = inventory.getSizeInventory(); i < count; i++) {
 			ItemStack book = inventory.getStackInSlot(i);
 			if (book != null && book.getItem() == Item.writtenBook) {
-				String toUsername = getUsernameFromBook(book);
-				if (toUsername != null) {
+				BookData bookData = getBookData(book);
+				if (bookData != null) {
 					foundValidBook = true;
+					sendBook(bookData);
+					inventory.setInventorySlotContents(i, null);
 				}
 			}
 		}
@@ -127,18 +97,23 @@ public class mod_CubeMail implements IMod, IPlayerBlockListener {
 		return foundValidBook;
 	}
 	
-	private static String getUsernameFromBook(ItemStack book) {
+	private static BookData getBookData(ItemStack book) {
 		NBTTagCompound tagCompound = book.getTagCompound();
 		if (ItemEditableBook.validBookTagContents(tagCompound)) {
-			//NBTTagString title = (NBTTagString)tagCompound.getTag("title");
-			//NBTTagString author = (NBTTagString)tagCompound.getTag("author");
+			NBTTagString title = (NBTTagString)tagCompound.getTag("title");
+			NBTTagString author = (NBTTagString)tagCompound.getTag("author");
 			NBTTagList pages = (NBTTagList)tagCompound.getTag("pages");
 			if (pages.tagCount() > 0) {
 				NBTTagString firstPage = (NBTTagString)pages.tagAt(0);
 				if (firstPage != null && firstPage.data != null) {
 					Matcher matcher = toLine.matcher(firstPage.data.trim());
 					if (matcher.matches()) {
-						return matcher.group(1);
+						String[] pagesArr = new String[pages.tagCount()];
+						for (int i = 0, count = pages.tagCount(); i < count; i++) {
+							pagesArr[i] = ((NBTTagString)pages.tagAt(i)).data;
+						}
+						
+						return new BookData(matcher.group(1), title.data, author.data, pagesArr);
 					}
 				}
 			}
@@ -147,7 +122,9 @@ public class mod_CubeMail implements IMod, IPlayerBlockListener {
 		return null;
 	}
 	
-	private void sendBook(String username, ItemStack book) {
-		
+	private void sendBook(BookData bookData) {
+		//NBTTagCompound tagCompound = book.getTagCompound();
+		//int count = data.getInt("to_" + username.toLowerCase(), "count", 0);
+		//tagCompound.
 	}
 }
