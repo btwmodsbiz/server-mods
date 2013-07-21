406e60d1f0e832634c14f0e7737404160c3077ca
Andre Mekkawi <github@andremekkawi.com>
2013-07-20 21:22:40 -0400

    Changed MessageUser to also include an optional gateway string, refactored JSON clean up from handleAsServer to a toJsonCleaned method, and removed handleAsServer and canSendMessage overrides to make it more generic.

--- src/btwmod/centralchat/MessageUser.java
+++ src/btwmod/centralchat/MessageUser.java
@@ -1,33 +1,34 @@
 package btwmod.centralchat;
 
-import org.java_websocket.WebSocket;
-
 import btwmods.Util;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public abstract class MessageUser extends Message {
+	
 	public final String username;
+	public final String gateway;
 	public final String color;
 	public final String alias;
 	
-	public MessageUser(String username) {
-		this(username, null, null);
-	}
-	
-	public MessageUser(String username, String color, String alias) {
+	public MessageUser(String username, String gateway, String color, String alias) {
 		this.username = username;
+		this.gateway = gateway;
 		this.color = color;
 		this.alias = alias;
 	}
 	
 	public MessageUser(JsonObject json) {
-		JsonElement color = json.get("color");
-		JsonElement alias = json.get("alias");
-		
 		this.username = json.get("username").getAsString();
+		
+		JsonElement gateway = json.get("gateway");
+		this.gateway = gateway != null && gateway.isJsonPrimitive() ? gateway.getAsString() : null;
+		
+		JsonElement color = json.get("color");
 		this.color = color != null && color.isJsonPrimitive() ? color.getAsString() : null;
+
+		JsonElement alias = json.get("alias");
 		this.alias = alias != null && alias.isJsonPrimitive() ? alias.getAsString() : null;
 	}
 
@@ -35,6 +36,9 @@ public abstract class MessageUser extends Message {
 	public JsonObject toJson() {
 		JsonObject obj = super.toJson();
 		obj.addProperty("username", this.username);
+		
+		if (this.gateway != null)
+			obj.addProperty("gateway", this.gateway);
 		
 		if (this.color != null)
 			obj.addProperty("color", this.color);
@@ -44,10 +48,28 @@ public abstract class MessageUser extends Message {
 		
 		return obj;
 	}
-
-	@Override
-	public boolean canSendMessage(ResourceConfig config) {
-		return config.clientType == ClientType.USER || super.canSendMessage(config);
+	
+	public JsonObject toJsonCleaned(IServer server, ResourceConfig config) {
+		JsonObject json = toJson();
+		
+		// Force user ID for those authenticated as users.
+		json.addProperty("username", config.clientType == ClientType.USER ? server.getActualUsername(config.id) : username);
+		
+		// USER clients should not have a gateway.
+		if (config.clientType == ClientType.USER && json.has("gateway"))
+			json.remove("gateway");
+		
+		// Set the user's chat color, if it has one.
+		json.addProperty("color", server.getChatColor(username));
+		if (json.get("color").isJsonNull())
+			json.remove("color");
+		
+		// Set the user's alias, if it has one.
+		json.addProperty("alias", server.getChatAlias(username));
+		if (json.get("alias").isJsonNull())
+			json.remove("alias");
+		
+		return json;
 	}
 	
 	public String getDisplayUsername(boolean withColor) {
@@ -61,25 +83,5 @@ public abstract class MessageUser extends Message {
 		
 		String colorChar = Message.getColorChar(color);
 		return colorChar == null ? displayName : colorChar + displayName + resetColorChar;
-	}
-
-	@Override
-	public void handleAsServer(IServer server, WebSocket conn, ResourceConfig config) {
-		JsonObject json = toJson();
-		
-		// Force user ID for those authenticated as users.
-		json.addProperty("username", config.clientType == ClientType.USER ? server.getActualUsername(config.id) : username);
-		
-		// Set the user's chat color, if it has one.
-		json.addProperty("color", server.getChatColor(username));
-		if (json.get("color").isJsonNull())
-			json.remove("color");
-		
-		// Set the user's alias, if it has one.
-		json.addProperty("alias", server.getChatAlias(username));
-		if (json.get("alias").isJsonNull())
-			json.remove("alias");
-		
-		server.sendToAll(json.toString());
 	}
 }
