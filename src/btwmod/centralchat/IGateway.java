27fe6e78c79cf5830484a88f6e267d9ee0b39150
Andre Mekkawi <github@andremekkawi.com>
2013-07-20 23:07:01 -0400

    Added getId and getUsernames to IGateway.

--- src/btwmod/centralchat/IGateway.java
+++ src/btwmod/centralchat/IGateway.java
@@ -1,8 +1,10 @@
 package btwmod.centralchat;
 
 public interface IGateway {
+	public String getId();
 	public void addRestorableChat(String chat);
 	public void onConnect(MessageConnect connect);
 	public void onDisconnect(MessageDisconnect disconnect);
 	public void setAlias(String username, String alias);
+	public String[] getUsernames();
 }
