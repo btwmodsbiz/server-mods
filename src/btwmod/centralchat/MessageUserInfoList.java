cac4e9cd6feea6b7f6d12869c0762d20cf7e8af1
Andre Mekkawi <github@andremekkawi.com>
2013-07-20 22:12:00 -0400

    Renamed MessageUserList to MessageUserInfoList and changed it to use a MessageUserInfo array instead of a nested class.

---
+++ src/btwmod/centralchat/MessageUserInfoList.java
@@ -0,0 +1,73 @@
+package btwmod.centralchat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.java_websocket.WebSocket;
+import btwmods.ChatAPI;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * A list of users connected to the server.
+ * Only sent from server to client when the client first connects.
+ * 
+ * @author amekkawi
+ */
+public class MessageUserInfoList extends Message {
+	
+	public static final String TYPE = "userlist";
+
+	@Override
+	public String getType() {
+		return TYPE;
+	}
+	
+	public final MessageUserInfo[] users;
+
+	public MessageUserInfoList(MessageUserInfo[] users) {
+		this.users = users;
+	}
+	
+	public MessageUserInfoList(JsonObject json) {
+		JsonArray usersArray = json.get("users").getAsJsonArray();
+		
+		users = new MessageUserInfo[usersArray.size()];
+		for (int i = 0, len = usersArray.size(); i < len; i++) {
+			users[i] = new MessageUserInfo(usersArray.get(0).getAsJsonObject());
+		}
+	}
+	
+	@Override
+	public JsonObject toJson() {
+		JsonObject obj = super.toJson();
+		
+		JsonArray userArray = new JsonArray();
+		for (MessageUserInfo user : users) {
+			userArray.add(user.toJson());
+		}
+		obj.add("users", userArray);
+		
+		return obj;
+	}
+
+	@Override
+	public void handleAsServer(IServer server, WebSocket conn, ResourceConfig config) {
+		// Does not handle this kind of message.
+	}
+	
+	@Override
+	public void handleAsGateway(IGateway gateway) {
+		Set<String> uniqueUsers = new HashSet<String>();
+		
+		for (MessageUserInfo user : users) {
+			gateway.setAlias(user.username, user.alias);
+			uniqueUsers.add(user.username.toLowerCase());
+		}
+		
+		int len = uniqueUsers.size();
+		if (len > 0)
+			ChatAPI.sendChatToAllPlayers(len + " user" + (len == 1 ? "" : "s") + " available via the chat server.");
+	}
+}
