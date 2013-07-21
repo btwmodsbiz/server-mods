6ad974f7a265944c17cd8986c3b3b3909006f694
Andre Mekkawi <github@andremekkawi.com>
2013-07-21 19:36:27 -0400

    Removed onConnect and onDisconnect from IGateway and added onSuccessfulConnect and onWaitForReconnect.

--- src/btwmod/centralchat/IGateway.java
+++ src/btwmod/centralchat/IGateway.java
@@ -1,13 +1,10 @@
 package btwmod.centralchat;
 
-import btwmod.centralchat.message.MessageConnect;
-import btwmod.centralchat.message.MessageDisconnect;
-
 public interface IGateway {
 	public String getId();
 	public void addRestorableChat(String chat);
-	public void onConnect(MessageConnect connect);
-	public void onDisconnect(MessageDisconnect disconnect);
 	public void setAlias(String username, String alias);
 	public String[] getUsernames();
+	public void onSuccessfulConnect();
+	public void onWaitForReconnect();
 }
